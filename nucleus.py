aa365c95e3a5aec23c872a956ade960544719260
mattmolo <molo.matt@gmail.com>
2016-07-23 18:28:59 -0700

    Initial performace testing

--- nucleus.py
+++ nucleus.py
@@ -1,8 +1,28 @@
+import time
 from flask import Flask, render_template
 from flask_socketio import SocketIO
 
 app = Flask(__name__)
 socketio = SocketIO(app)
+sum = 0.0
+total = 0.0
+lt = time.time()
 
-if __name__ == '__main__':
-    socketio.run(app)
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@socketio.on('status')
+def status(data):
+    now = time.time()
+    global sum
+    global total
+    global lt
+
+    sum += (now - lt)
+    total += 1.0
+    print (now - lt) * 1000.0, (sum / total) * 1000.0
+    lt = now
+    socketio.emit('status', {'data': 'got it!'})
+
+socketio.run(app)
